b43ed2f533debe7753446c8471bbbf606fc5bcdb
Valentin Serrago <valentinserrago@gmail.com>
2013-05-30 15:53:08 +0000

    Added TimeStampUnitTest.cs, deleted UnitTest1.cs

---
+++ EnACTUnitTestProject/TimeStampUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EnACT;
+
+
+namespace EnACTUnitTestProject
+{
+    [TestClass]
+    public class TimeStampUnitTest
+    {
+        [TestMethod]
+        public void AssignmentTest()
+        {
+            //Arrange
+            double d = 5;
+            string s = "00:00:05.0";
+
+            double expected = 5;
+            
+            Timestamp td;
+            Timestamp ts;
+
+            //Act
+            td = new Timestamp();
+            td = d;
+
+            ts = new Timestamp();
+            ts = s;
+
+            //Assert
+            Assert.AreEqual(expected, td.AsDouble);
+            Assert.AreEqual(expected, ts.AsDouble);
+        }
+    }
+}
