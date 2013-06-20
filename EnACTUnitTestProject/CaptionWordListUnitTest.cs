135acb63aac71959050e14b65692b922dd410703
Valentin Serrago <valentinserrago@gmail.com>
2013-06-20 15:44:51 -0400

    Add CaptionWordListUnitTest

---
+++ EnACTUnitTestProject/CaptionWordListUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnACTUnitTestProject
+{
+    /// <summary>
+    /// Contains Tests for the CaptionWordList class.
+    /// </summary>
+    [TestClass]
+    public class CaptionWordListUnitTest
+    {
+        /// <summary>
+        /// Tests the Feed method of CaptionWordList.
+        /// </summary>
+        [TestMethod]
+        public void FeedTest()
+        {
+        }
+    }
+}
