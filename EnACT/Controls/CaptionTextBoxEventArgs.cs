802038a9862e42500ba85dc75e8cce50987101b1
Valentin Serrago <valentinserrago@gmail.com>
2013-07-03 13:00:19 -0400

    Refactor EventArgs.cs
    Renames EventArgs.cs to TimelineEventArgs.cs, and moves the
    CaptionWordSelectedEventArgs class into CaptionTextBoxEventArgs.cs.

---
+++ EnACT/Controls/CaptionTextBoxEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnACT
+{
+    #region CaptionWordSelectedEventArgs
+    /// <summary>
+    /// Event Arguments for the CaptionTextBox.CaptionWordSelected event.
+    /// </summary>
+    public class CaptionWordSelectedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The word selected by the user in CaptionTextBox.
+        /// </summary>
+        public CaptionWord SelectedWord { private set; get; }
+
+        /// <summary>
+        /// Constructs a CaptionWordSelectedEventArgs with a specified CaptionWord.
+        /// </summary>
+        /// <param name="selectedWord">The selected CaptionWord.</param>
+        public CaptionWordSelectedEventArgs(CaptionWord selectedWord)
+        {
+            this.SelectedWord = selectedWord;
+        }
+    }
+    #endregion
+}
--- EnACT/Controls/TimelineEventArgs.cs
+++ EnACT/Controls/TimelineEventArgs.cs
@@ -40,26 +40,4 @@ namespace EnACT
         }
     }
     #endregion
-
-    #region CaptionWordSelectedEventArgs
-    /// <summary>
-    /// Event Arguments for the CaptionTextBox.CaptionWordSelected event.
-    /// </summary>
-    public class CaptionWordSelectedEventArgs : EventArgs
-    {
-        /// <summary>
-        /// The word selected by the user in CaptionTextBox.
-        /// </summary>
-        public CaptionWord SelectedWord { private set; get; }
-
-        /// <summary>
-        /// Constructs a CaptionWordSelectedEventArgs with a specified CaptionWord.
-        /// </summary>
-        /// <param name="selectedWord">The selected CaptionWord.</param>
-        public CaptionWordSelectedEventArgs(CaptionWord selectedWord)
-        {
-            this.SelectedWord = selectedWord;
-        }
-    }
-    #endregion
 }//Namespace
