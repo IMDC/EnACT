e37bc472bfc9fa44ef9e875ddb5ee66d2480d98d
Valentin Serrago <valentinserrago@gmail.com>
2013-07-25 13:48:16 -0400

    Add XMLStrings.cs
    Provides string constants for writing and reading xml methods.

---
+++ EnACT/Core/XMLStrings.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnACT
+{
+    /// <summary>
+    /// A static class that contains XML element names.
+    /// </summary>
+    public static class XMLElements
+    {
+        public const string Enact      = "enact";
+        public const string Settings   = "settings";
+        public const string Meta       = "meta";
+        public const string Playback   = "playback";
+        public const string Skin       = "skin";
+        public const string Video      = "video";
+        public const string Emotions   = "emotions";
+        public const string Happy      = "happy";
+        public const string Sad        = "sad";
+        public const string Fear       = "fear";
+        public const string Anger      = "anger";
+        public const string Speakers   = "speakers";
+        public const string Speaker    = "speaker";
+        public const string Background = "background";
+        public const string Font       = "font";
+        public const string Captions   = "captions";
+        public const string Caption    = "capttion";
+        public const string Word       = "word";
+    }
+
+    /// <summary>
+    /// A static class that contains XML attribute names.
+    /// </summary>
+    public static class XMLAttributes
+    {
+        public const string Base                 = "base";
+        public const string WordSpacing          = "wordSpacing";
+        public const string SeparateEmotionWords = "separateEmotionWords";
+        public const string AutoPlay             = "autoPlay";
+        public const string AutoRewind           = "autoRewind";
+        public const string Seek                 = "seek";
+        public const string AutoSize             = "autoSize";
+        public const string Scale                = "scale";
+        public const string Volume               = "volume";
+        public const string ShowCaptions         = "showCaptions";
+        public const string Source               = "src";
+        public const string AutoHide             = "autoHide";
+        public const string FadeTime             = "fadeTime";
+        public const string BackgroundAlpha      = "backgroundAlpha";
+        public const string BackgroundColour     = "backgroundColour";
+        public const string FPS                  = "fps";
+        public const string Duration             = "duration";
+        public const string AlphaBegin           = "alphaBegin";
+        public const string AlphaFinish          = "alphaFinish";
+        public const string ScaleBegin           = "scaleBegin";
+        public const string ScaleFinish          = "scaleFinish";
+        public const string YFinish              = "yFinish";
+        public const string VibrateX             = "vibrateX";
+        public const string VibrateY             = "vibrateY";
+        public const string Name                 = "name";
+        public const string Visible              = "visible";
+        public const string Alpha                = "alpha";
+        public const string Colour               = "colour";
+        public const string Size                 = "size";
+        public const string Bold                 = "bold";
+        public const string Begin                = "begin";
+        public const string End                  = "end";
+        public const string Speaker              = "speaker";
+        public const string Location             = "location";
+        public const string Align                = "align";
+        public const string Emotion              = "emotion";
+        public const string Intensity            = "intensity";
+    }
+}
