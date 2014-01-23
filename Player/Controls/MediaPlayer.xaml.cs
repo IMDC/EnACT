1703a9f1f00cde3f5268faa7cd28e708237cdb12
Valentin Serrago <valentinserrago@gmail.com>
2014-01-23 14:16:04 -0500

    Remove animation generation code from MediaPlayer

--- Player/Controls/MediaPlayer.xaml.cs
+++ Player/Controls/MediaPlayer.xaml.cs
@@ -59,106 +59,19 @@ namespace Player.Controls
 
             storyboard.Children.Add(visibilityAnimation);
 
-            //Duration for all animations
-            Duration duration = TimeSpan.FromSeconds(0.6);
-            Duration halfDuration = TimeSpan.FromSeconds(0.6/2);
+            AnimationSet a = new AnimationSet(c, t);
 
-            TextEffect e1 = new TextEffect
+            /*
+            foreach (TextEffect effect in a.TextEffects)
             {
-                PositionStart = 0,
-                PositionCount = t.Text.Length,
-            };
+                t.TextEffects.Add(effect);
+            }
 
-
-            var formattedText = new FormattedText(
-                t.Text,
-                CultureInfo.CurrentUICulture,
-                FlowDirection.LeftToRight,
-                new Typeface(t.FontFamily, t.FontStyle, t.FontWeight, t.FontStretch),
-                t.FontSize,
-                Brushes.Black);
-
-            ScaleTransform st = new ScaleTransform 
+            foreach (AnimationTimeline animation in a.Animations)
             {
-                CenterX = formattedText.Width/2,
-                CenterY = formattedText.Height,
-            };
-            e1.Transform = st;
-
-            t.TextEffects.Add(e1);
-
-            DoubleAnimationUsingKeyFrames d1 = new DoubleAnimationUsingKeyFrames
-            {
-                BeginTime = TimeSpan.FromSeconds(c.Begin),
-                Duration = duration,
-            };
-
-            d1.KeyFrames.Add(new LinearDoubleKeyFrame(0.5, KeyTime.FromPercent(0)));
-            d1.KeyFrames.Add(new LinearDoubleKeyFrame(1.3, KeyTime.FromPercent(0.5)));
-            d1.KeyFrames.Add(new LinearDoubleKeyFrame(1, KeyTime.FromPercent(1)));
-
-            Storyboard.SetTargetName(d1,t.Name);
-            Storyboard.SetTargetProperty(d1, new PropertyPath("TextEffects[0].Transform.ScaleX"));
-
-            storyboard.Children.Add(d1);
-
-            //Make second animation for y scale
-            DoubleAnimationUsingKeyFrames d2 = d1.Clone();
-
-            Storyboard.SetTargetName(d2, t.Name);
-            Storyboard.SetTargetProperty(d2, new PropertyPath("TextEffects[0].Transform.ScaleY"));
-
-            storyboard.Children.Add(d2);
-
-            TextEffect e2 = new TextEffect //Yfinish effect
-            {
-                PositionStart = 0,
-                PositionCount = t.Text.Length,
-                Transform = new TranslateTransform(),
-            };
-
-            t.TextEffects.Add(e2);
-
-            DoubleAnimationUsingKeyFrames d3 = new DoubleAnimationUsingKeyFrames
-            {
-                BeginTime = TimeSpan.FromSeconds(c.Begin),
-                Duration = duration,
-            };
-
-            d3.KeyFrames.Add(new LinearDoubleKeyFrame(0, KeyTime.FromPercent(0)));
-            d3.KeyFrames.Add(new LinearDoubleKeyFrame(-10, KeyTime.FromPercent(0.5)));
-            d3.KeyFrames.Add(new LinearDoubleKeyFrame(0, KeyTime.FromPercent(1)));
-
-            Storyboard.SetTargetName(d3, t.Name);
-            Storyboard.SetTargetProperty(d3, new PropertyPath("TextEffects[1].Transform.Y"));
-
-            storyboard.Children.Add(d3);
-
-            //alpha effect
-            TextEffect e3 = new TextEffect()
-            {
-                PositionStart = 0,
-                PositionCount = t.Text.Length,
-            };
-
-            e3.Foreground = new SolidColorBrush(Colors.White);
-            
-            t.TextEffects.Add(e3);
-
-            ColorAnimationUsingKeyFrames c1 = new ColorAnimationUsingKeyFrames
-            {
-                BeginTime = TimeSpan.FromSeconds(c.Begin),
-                Duration = duration,
-            };
-
-            c1.KeyFrames.Add(new LinearColorKeyFrame(Colors.White, KeyTime.FromPercent(0)));
-            c1.KeyFrames.Add(new LinearColorKeyFrame(Color.FromArgb(100, 255, 255, 255), KeyTime.FromPercent(0.5)));
-            c1.KeyFrames.Add(new LinearColorKeyFrame(Colors.White, KeyTime.FromPercent(1)));
-
-            Storyboard.SetTargetName(c1, t.Name);
-            Storyboard.SetTargetProperty(c1, new PropertyPath("TextEffects[2].Foreground." + SolidColorBrush.ColorProperty.ToString()));
-
-            storyboard.Children.Add(c1);
+                storyboard.Children.Add(animation);
+            }
+            //*/
         }
 
         /// <summary>
