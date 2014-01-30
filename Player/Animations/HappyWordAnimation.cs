1da6ed89bdeefd54e5cc6898c014760c9ebb6afb
Valentin Serrago <valentinserrago@gmail.com>
2014-01-30 12:02:38 -0500

    Refactor Code

--- Player/Animations/HappyWordAnimation.cs
+++ Player/Animations/HappyWordAnimation.cs
@@ -15,11 +15,14 @@ namespace Player.Animations
     public class HappyWordAnimation : WordAnimation
     {
         #region Constructor
+
         /// <summary>
         /// Creates a new instance of a HappyWordAnimation. Initializes the animation based on the
         /// specified paramaters.
         /// </summary>
         /// <param name="w">The CaptionWord to base the animation off of.</param>
+        /// <param name="beginIndex">The index of the caption string at which the CaptionWord
+        /// begins.</param>
         /// <param name="c">The Caption that the CaptionWord belongs to.</param>
         /// <param name="t">The Textblock that this animation will be applied to.</param>
         public HappyWordAnimation(CaptionWord w, int beginIndex, Caption c, TextBlock t) : base(w, beginIndex, c, t)
@@ -28,45 +31,46 @@ namespace Player.Animations
             Duration duration = TimeSpan.FromSeconds(0.6);
             Duration halfDuration = TimeSpan.FromSeconds(0.6 / 2);
 
-            TextEffect e1 = new TextEffect
-            {
-                PositionStart = beginIndex,
-                PositionCount = w.Length,
-            };
-
-            var formattedText = new FormattedText(
+            var formattedText = new FormattedText
+            (
                 w.Text,
                 CultureInfo.CurrentUICulture,
                 FlowDirection.LeftToRight,
                 new Typeface(t.FontFamily, t.FontStyle, t.FontWeight, t.FontStretch),
                 t.FontSize,
-                Brushes.Black);
+                Brushes.Black
+            );
 
-            ScaleTransform st = new ScaleTransform
+            TextEffect e1 = new TextEffect
             {
-                CenterX = formattedText.Width / 2,
-                CenterY = formattedText.Height,
+                PositionStart = beginIndex,
+                PositionCount = w.Length,
+                Transform = new ScaleTransform
+                {
+                    //TODO: Fix centering
+                    CenterX = formattedText.Width / 2,
+                    CenterY = formattedText.Height,
+                },
             };
-            e1.Transform = st;
 
             this.TextEffects.Add(e1);
 
-            DoubleAnimationUsingKeyFrames d1 = new DoubleAnimationUsingKeyFrames
+            DoubleAnimationUsingKeyFrames a1 = new DoubleAnimationUsingKeyFrames
             {
                 BeginTime = TimeSpan.FromSeconds(c.Begin),
                 Duration = duration,
             };
 
-            d1.KeyFrames.Add(new LinearDoubleKeyFrame(0.5, KeyTime.FromPercent(0)));
-            d1.KeyFrames.Add(new LinearDoubleKeyFrame(1.3, KeyTime.FromPercent(0.5)));
-            d1.KeyFrames.Add(new LinearDoubleKeyFrame(1, KeyTime.FromPercent(1)));
+            a1.KeyFrames.Add(new LinearDoubleKeyFrame(0.5, KeyTime.FromPercent(0)));
+            a1.KeyFrames.Add(new LinearDoubleKeyFrame(1.3, KeyTime.FromPercent(0.5)));
+            a1.KeyFrames.Add(new LinearDoubleKeyFrame(1, KeyTime.FromPercent(1)));
 
-            this.Animations.Add(d1);
+            this.Animations.Add(a1);
 
             //Make second animation for y scale
-            DoubleAnimationUsingKeyFrames d2 = d1.Clone();
+            DoubleAnimationUsingKeyFrames a2 = a1.Clone();
 
-            this.Animations.Add(d2);
+            this.Animations.Add(a2);
 
             TextEffect e2 = new TextEffect //Yfinish effect
             {
@@ -77,17 +81,17 @@ namespace Player.Animations
 
             this.TextEffects.Add(e2);
 
-            DoubleAnimationUsingKeyFrames d3 = new DoubleAnimationUsingKeyFrames
+            DoubleAnimationUsingKeyFrames a3 = new DoubleAnimationUsingKeyFrames
             {
                 BeginTime = TimeSpan.FromSeconds(c.Begin),
                 Duration = duration,
             };
 
-            d3.KeyFrames.Add(new LinearDoubleKeyFrame(0, KeyTime.FromPercent(0)));
-            d3.KeyFrames.Add(new LinearDoubleKeyFrame(-10, KeyTime.FromPercent(0.5)));
-            d3.KeyFrames.Add(new LinearDoubleKeyFrame(0, KeyTime.FromPercent(1)));
+            a3.KeyFrames.Add(new LinearDoubleKeyFrame(0, KeyTime.FromPercent(0)));
+            a3.KeyFrames.Add(new LinearDoubleKeyFrame(-10, KeyTime.FromPercent(0.5)));
+            a3.KeyFrames.Add(new LinearDoubleKeyFrame(0, KeyTime.FromPercent(1)));
 
-            this.Animations.Add(d3);
+            this.Animations.Add(a3);
 
             //alpha effect
             TextEffect e3 = new TextEffect()
@@ -100,17 +104,17 @@ namespace Player.Animations
 
             this.TextEffects.Add(e3);
 
-            ColorAnimationUsingKeyFrames c1 = new ColorAnimationUsingKeyFrames
+            ColorAnimationUsingKeyFrames a4 = new ColorAnimationUsingKeyFrames
             {
                 BeginTime = TimeSpan.FromSeconds(c.Begin),
                 Duration = duration,
             };
 
-            c1.KeyFrames.Add(new LinearColorKeyFrame(Colors.White, KeyTime.FromPercent(0)));
-            c1.KeyFrames.Add(new LinearColorKeyFrame(Color.FromArgb(100, 255, 255, 255), KeyTime.FromPercent(0.5)));
-            c1.KeyFrames.Add(new LinearColorKeyFrame(Colors.White, KeyTime.FromPercent(1)));
+            a4.KeyFrames.Add(new LinearColorKeyFrame(Colors.White, KeyTime.FromPercent(0)));
+            a4.KeyFrames.Add(new LinearColorKeyFrame(Color.FromArgb(100, 255, 255, 255), KeyTime.FromPercent(0.5)));
+            a4.KeyFrames.Add(new LinearColorKeyFrame(Colors.White, KeyTime.FromPercent(1)));
 
-            this.Animations.Add(c1);
+            this.Animations.Add(a4);
         }
         #endregion
 
